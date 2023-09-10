475187769f707f31c3020131367dfce0dbf85c2d
Naryond <giuggey85@gmail.com>
2023-09-10 16:27:14 +0100

    button line fixed

--- components/FoodList.js
+++ components/FoodList.js
@@ -4,11 +4,11 @@ import Modal from '@/components/Modal';
 
 function FoodList({ food, deleteFood, buyFood }) {
   const [isChecked, setIsChecked] = useState(food.home);
-  const [isDeleting, setIsDeleting] = useState(false)
+  const [isDeleting, setIsDeleting] = useState(false);
 
   const deletingHandler = () => {
-    setIsDeleting(prevState => !prevState)
-  }
+    setIsDeleting((prevState) => !prevState);
+  };
 
   const erase = () => {
     deleteFood(food);
@@ -21,23 +21,25 @@ function FoodList({ food, deleteFood, buyFood }) {
 
   return (
     <>
-    {isDeleting ? <Modal onErase={erase} onCancel={deletingHandler} /> : null}
-    <div className="food-list">
-      <input
-        id={food.id}
-        type="checkbox"
-        name="home"
-        checked={isChecked}
-        onChange={handleOnChange}
-      />
-      <label className="food-list__name" htmlFor={food.id}>
-        {food.name}
-      </label>
-      <div className="food-list__shop">{food.shop}</div>
-      <button className="button__delete pointer" onClick={deletingHandler}>
-        x
-      </button>
-    </div>
+      {isDeleting ? <Modal onErase={erase} onCancel={deletingHandler} /> : null}
+      <div className="food-list">
+        <input
+          id={food.id}
+          type="checkbox"
+          name="home"
+          checked={isChecked}
+          onChange={handleOnChange}
+        />
+        <label className="food-list__name" htmlFor={food.id}>
+          {food.name}
+        </label>
+        <div className='food-list__button'>
+          <div className="food-list__shop">{food.shop}</div>
+          <button className="button__delete pointer" onClick={deletingHandler}>
+            x
+          </button>
+        </div>
+      </div>
     </>
   );
 }
