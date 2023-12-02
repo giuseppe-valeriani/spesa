9222a715ea9b8676ecc304ac57ac422bf305d10c
Giuseppe Valeriani <giuggey85@gmail.com>
2023-12-02 16:35:31 +0000

    wip firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId:process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
+  apiKey: "AIzaSyBU5R6f2CRDy2YzzTBIAXm3S--M7OaPAgo",
+  authDomain: "spesa-44b4d.firebaseapp.com",
+  projectId: "spesa-44b4d",
+  storageBucket: "spesa-44b4d.appspot.com",
+  messagingSenderId: "1015218529106",
+  appId: "1:1015218529106:web:ba01a73c5edb8b415f508b"
 };
 
 const app = initializeApp(firebaseConfig);
